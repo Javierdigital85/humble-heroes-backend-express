e8b5fa8c861c5d0391453f1f590cd2aac5a11ffc
javiercolodro <javiercolodro@gmail.com>
2025-02-10 21:34:43 -0300

    update superheroes.controller

--- src/controller/superheroes.controller.ts
+++ src/controller/superheroes.controller.ts
@@ -3,28 +3,30 @@ import * as heroesService from "../service/superheroes.service";
 
 export const newHeroe: RequestHandler = async (req, res) => {
   try {
-    const { name } = req.body;
+    const { name, superpower, humilityScore } = req.body;
     const findHero = await heroesService.findHeroe(name);
     if (findHero) {
-      res.status(400).json({ message: "Heroe already exists!" });
+      res.status(409).json({ message: "Heroe already exists!" });
       return;
     }
-    const createHeroe = await heroesService.createHeroe(req.body);
-    res.status(200).json(createHeroe);
-  } catch (error) {
-    if ((error as any).name === "SequelizeValidationError") {
-      const validationErrors = (error as any).errors.map((e: any) => e.message);
-
-      if (
-        validationErrors.some((msg: string) => msg.includes("humilityScore"))
-      ) {
-        res.status(400).json({
-          error: "Validation error",
-          message: "The value of humility score must be between 1 and 10.",
-        });
-        return;
-      }
+    if (!name) {
+      res.status(400).json({ message: "Heroe needs a name" });
+      return;
     }
+    if (!superpower) {
+      res.status(400).json({ message: "Heroe needs superpower" });
+      return;
+    }
+    if (!humilityScore) {
+      res.status(400).json({
+        message: "The value of humility score must be between 1 and 10.",
+      });
+      return;
+    }
+
+    const createHeroe = await heroesService.createHeroe(req.body);
+    res.status(201).json(createHeroe);
+  } catch (error) {
     res.status(500).json({ message: "internal server error" });
   }
 };
